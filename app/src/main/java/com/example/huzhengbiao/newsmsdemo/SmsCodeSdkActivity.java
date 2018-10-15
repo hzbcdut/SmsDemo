26418fdb96be213749c12de69c4d3de8dbd441b1
huzhengbiao <hzhengbiao@126.com>
2018-10-15 17:03:39 +0800

    使用开源的一样不可以使用， 应该也是用系统的。 Android 8.0同样获取不到。

---
+++ app/src/main/java/com/example/huzhengbiao/newsmsdemo/SmsCodeSdkActivity.java
@@ -0,0 +1,73 @@
+package com.example.huzhengbiao.newsmsdemo;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.stfalcon.smsverifycatcher.OnSmsCatchListener;
+import com.stfalcon.smsverifycatcher.SmsVerifyCatcher;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SmsCodeSdkActivity extends AppCompatActivity {
+
+    public static final String TAG = SmsCodeSdkActivity.class.getSimpleName();
+
+    private SmsVerifyCatcher smsVerifyCatcher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sms_code_sdk);
+
+        smsVerifyCatcher = new SmsVerifyCatcher(this, new OnSmsCatchListener<String>() {
+            @Override
+            public void onSmsCatch(String message) {
+                String code = parseCode(message);//Parse verification code
+
+                LogUtil.logInfo("debug", TAG + "---> code = " + code);
+                //then you can send verification code to server
+            }
+        });
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        smsVerifyCatcher.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        smsVerifyCatcher.onStop();
+    }
+
+    /**
+     * need for Android 6 real time permissions
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        smsVerifyCatcher.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
+    /**
+     * Parse verification code
+     *
+     * @param message sms message
+     * @return only four numbers from massage string
+     */
+    private String parseCode(String message) {
+        Pattern p = Pattern.compile("\\b\\d{4}\\b");
+        Matcher m = p.matcher(message);
+        String code = "";
+        while (m.find()) {
+            code = m.group(0);
+        }
+        return code;
+    }
+}
